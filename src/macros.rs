36a7c5c60cf822d2ef61c1d73744be6ca4409e29
iamgabrielsoft <iamgabrielsoft@gmail.com>
2022-07-23 02:19:34 +0100

    macro file for the project

---
+++ src/macros.rs
@@ -0,0 +1,36 @@
+
+
+#[macro_export]
+//setter macro to re-call every function on the status file
+macro_rules! setter {
+    ($func:ident, $field:ident, $t:ty) => {
+        pub fn $func(&mut self, $field: $t) {
+            if self.$field != $field {
+                self.redraw = true; 
+                self.$field = $field; 
+            }
+        }
+    };
+
+    ($func:ident, $field:ident, $t:ty, $conv:expr) => {
+        pub fn $func(&mut self, $field: $t) {
+            if self.$field != $field {
+                self.redraw = true; 
+                self.$field = $conv; 
+            }
+        }
+    }
+}
+
+
+
+#[macro_export]
+/* call color base on the macro */
+macro_rules! regent_color {
+    (fg, $r:expr, $g: expr, $b: expr) => {
+        concat!("\x1b[38;2;", $r, ';', $g, ';', $b, "m")
+    };
+    (bg, $r:expr, $g: expr, $b: expr) => {
+        concat!("\x1b[48;2;", $r, ';', $g, ';', $b, "m")
+    }
+}
